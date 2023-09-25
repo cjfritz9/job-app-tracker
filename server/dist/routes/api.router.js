1f1f800259b0894c77836df4ab07769711da59dd
CJ <cjfritz9@gmail.com>
2023-09-25 14:24:26 -0600

    fixed router method from get to use

--- server/dist/routes/api.router.js
+++ server/dist/routes/api.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
 const apiRouter = express.Router();
 import jobAppRouter from './jobApps.router.js';
-apiRouter.get('/job-apps', jobAppRouter);
+apiRouter.use('/job-apps', jobAppRouter);
 export default apiRouter;
--- server/src/routes/api.router.ts
+++ server/src/routes/api.router.ts
@@ -3,6 +3,6 @@ import express from 'express';
 const apiRouter = express.Router();
 
 import jobAppRouter from './jobApps.router.js';
-apiRouter.get('/job-apps', jobAppRouter);
+apiRouter.use('/job-apps', jobAppRouter);
 
 export default apiRouter;
